2431a44339b9cc8a3bd0b720dc4d4cf3d895022b
chinguyen98 <dacchi6761@gmail.com>
2020-09-06 19:55:52 +0700

    set up temp homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './App.scss';
+import './atomic.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App d-flex justify-content-center align-items-center">
       <header className="App-header">
         <h1>Hello Fine-Social-Gui</h1>
+        <p>This is My HOMEPAGE. U must login to see this!</p>
       </header>
     </div>
   );
